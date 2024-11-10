13073a8e4cf1c7343eed041d4fbb0a36c59e4a80
Harshkesharwani789 <atul123564@gmail.com>
2024-11-11 01:56:46 +0530

    fixed animations

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -6,23 +6,20 @@ import gsap from "gsap";
 function Canvas({ details }) {
   const { startIndex, numImages, duration, size, top, left, zIndex } = details;
 
-  const [index, setIndex] = useState(startIndex);
+  const [index, setIndex] = useState({ value: startIndex });
   const canvasRef = useRef(null);
 
-  // Use GSAP for animation control
   useGSAP(() => {
-    // Animate the index state
     gsap.to(index, {
       value: startIndex + numImages - 1,
       duration: duration,
       repeat: -1,
       ease: "linear",
       onUpdate: () => {
-        setIndex(Math.round(index)); // Update the index state on every frame
+        setIndex({ value: Math.round(index.value) });
       },
     });
 
-    // Animate canvas fade-in effect
     gsap.from(canvasRef.current, {
       opacity: 0,
       duration: 1,
@@ -31,24 +28,21 @@ function Canvas({ details }) {
   });
 
   useEffect(() => {
-    // Adjust canvas size based on device pixel ratio
     const scale = window.devicePixelRatio;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const img = new Image();
-    img.src = canvasImages[index];
-
+    img.src = canvasImages[index.value];
     img.onload = () => {
       canvas.width = canvas.offsetWidth * scale;
       canvas.height = canvas.offsetHeight * scale;
-      canvas.style.width = `${canvas.offsetWidth}px`;
-      canvas.style.height = `${canvas.offsetHeight}px`;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
 
-      // Ensure image is drawn with correct scaling
       ctx.scale(scale, scale);
       ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
     };
-  }, [index]); // Redraw image when index changes
+  }, [index]);
 
   return (
     <canvas
@@ -57,8 +51,8 @@ function Canvas({ details }) {
       ref={canvasRef}
       className="absolute"
       style={{
-        width: `${size * 1.7}px`,
-        height: `${size * 1.7}px`,
+        width: `${size * 1.8}px`,
+        height: `${size * 1.8}px`,
         top: `${top}%`,
         left: `${left}%`,
         zIndex: `${zIndex}`,
